2b35993979a29bfbe7592221e0cca69d5c265433
saul <ruizfernandezsaul@gmail.com>
2023-02-09 20:33:05 +0000

    Mejoras al archivo .ino

--- algoritmo_seguidor_linea/seguidor_de_linea_v1.ino
+++ algoritmo_seguidor_linea/seguidor_de_linea_v1.ino
@@ -9,8 +9,6 @@
    @copyright Copyright (c) 2023
 */
 
-#include <Stepper.h>
-
 /**
    @brief Pasa un valor a un intervalo determinado
 
@@ -37,15 +35,18 @@ int lectura_analog_s3{0}; // Sensor 3
 int lectura_analog_s4{0}; // Sensor 4
 int lectura_analog_s5{0}; // Sensor 5
 int lectura_analog_s6{0}; // Sensor 6
+int lectura_analog_s7{0}; // Sensor 7
+int lectura_analog_s8{0}; // Sensor 8
 
-const double kPesoS16{1.0};   // Pesos sensores 1 y 6
-const double kPesoS25{0.5};   // Pesos sensores 2 y 5
-const double kPesoS34{0.25};  // Pesos sensores 3 y 4
+const double kPesoS18{1.0};   // Pesos sensores 1 y 8
+const double kPesoS27{0.5};   // Pesos sensores 2 y 7
+const double kPesoS36{0.25};  // Pesos sensores 3 y 6
+const double kPeso45{0.1};    // Pesos sensores 4 y 5
 
 double suma_sector_izquierdo{0.0};
 double suma_sector_derecho{0.0};
 
-const double kVelocidadBase{50.0}; // RPM para el motor stepper que se utilize en el proyecto
+const double kVelocidadBase{20.0}; // RPM para el motor stepper que se utilize en el proyecto
 
 double factor_multiplicativo1{0.0};  // Factor multiplicativo sector izquierdo
 double factor_multiplicativo2{0.0};  // Factor multiplicativo sector derecho
@@ -60,35 +61,43 @@ double factor_correccion_errores2{0.0};  // Factor de corrección de errores mot
 
 int contador_iteraciones{0};
 
-Stepper myStepper1(kVelocidadBase, 8, 9, 10, 11); // Configuración de pines del motor izquierdo
-Stepper myStepper2(kVelocidadBase, 4, 5, 6, 7); // Configuración de pines del motor derecho
-
 void setup() {
 
   Serial.begin(9600);
 
-  pinMode(A0, INPUT);  // Sensor 1
-  pinMode(A1, INPUT);  // Sensor 2
-  pinMode(A2, INPUT);  // Sensor 3
-  pinMode(A3, INPUT);  // Sensor 4
-  pinMode(A4, INPUT);  // Sensor 5
-  pinMode(A5, INPUT);  // Sensor 6
+  pinMode(A3, INPUT);  // Sensor 1
+  pinMode(A2, INPUT);  // Sensor 2
+  pinMode(A1, INPUT);  // Sensor 3
+  pinMode(A0, INPUT);  // Sensor 4
+  pinMode(A7, INPUT);  // Sensor 5
+  pinMode(A6, INPUT);  // Sensor 6
+  pinMode(A5, INPUT);  // Sensor 7
+  pinMode(A4, INPUT);  // Sensor 8
+
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
 
   // Mapeamos las lecturas analógicas para pasarlas a un intervalo de [0,100]
-  lectura_analog_s1 = mapeo(analogRead(A0), 0, 1023, 0, 100);
-  lectura_analog_s2 = mapeo(analogRead(A1), 0, 1023, 0, 100);
-  lectura_analog_s3 = mapeo(analogRead(A2), 0, 1023, 0, 100);
-  lectura_analog_s4 = mapeo(analogRead(A3), 0, 1023, 0, 100);
-  lectura_analog_s5 = mapeo(analogRead(A4), 0, 1023, 0, 100);
-  lectura_analog_s6 = mapeo(analogRead(A5), 0, 1023, 0, 100);
+  lectura_analog_s1 = mapeo(analogRead(A3), 0, 1023, 0, 100);
+  lectura_analog_s2 = mapeo(analogRead(A2), 0, 1023, 0, 100);
+  lectura_analog_s3 = mapeo(analogRead(A1), 0, 1023, 0, 100);
+  lectura_analog_s4 = mapeo(analogRead(A0), 0, 1023, 0, 100);
+  lectura_analog_s5 = mapeo(analogRead(A7), 0, 1023, 0, 100);
+  lectura_analog_s6 = mapeo(analogRead(A6), 0, 1023, 0, 100);
+  lectura_analog_s7 = mapeo(analogRead(A5), 0, 1023, 0, 100);
+  lectura_analog_s8 = mapeo(analogRead(A4), 0, 1023, 0, 100);
 
-  suma_sector_izquierdo = (lectura_analog_s1 * kPesoS16) + (lectura_analog_s2 * kPesoS25) +
-                          (lectura_analog_s3 * kPesoS34);
-  suma_sector_derecho = (lectura_analog_s4 * kPesoS34) + (lectura_analog_s5 * kPesoS25) +
-                        (lectura_analog_s6 * kPesoS16);
+  suma_sector_izquierdo = (lectura_analog_s1 * kPesoS18) + (lectura_analog_s2 * kPesoS27) +
+                          (lectura_analog_s3 * kPesoS36) + (lectura_analog_s4 * kPeso45);
+  suma_sector_derecho = (lectura_analog_s5 * kPeso45) + (lectura_analog_s6 * kPesoS36) +
+                        (lectura_analog_s7 * kPesoS27) + (lectura_analog_s8 * kPesoS18);
 
   // Obtenemos los factores multiplicativos de las velocidades de cada motor
   factor_multiplicativo1 = mapeo(suma_sector_izquierdo, 0, 175, 1, 1.5);
@@ -114,10 +123,16 @@ void loop() {
   // Actualizamos la velocidad en cada motor
   velocidad_motor_izquierdo = kVelocidadBase * factor_multiplicativo1 * factor_correccion_errores1;
   velocidad_motor_derecho = kVelocidadBase * factor_multiplicativo2 * factor_correccion_errores2;
-  myStepper1.setSpeed(velocidad_motor_izquierdo);
-  myStepper2.setSpeed(velocidad_motor_derecho);
-  myStepper1.step((kVelocidadBase / 500) * velocidad_motor_izquierdo);
-  myStepper2.step((kVelocidadBase / 500) * velocidad_motor_derecho);
+  velocidad_motor_izquierdo = mapeo(velocidad_motor_izquierdo, kVelocidadBase, 50, 0, 1023);
+  velocidad_motor_derecho = mapeo(velocidad_motor_derecho, kVelocidadBase, 50, 0, 1023);
+
+  analogWrite(5, velocidad_motor_derecho);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+
+  analogWrite(6, velocidad_motor_derecho);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, LOW);
 
   error_anterior_cometido = error_cometido;
 }
